b9ad2812b7c8badf5d00da9162f0cfa4b8e63408
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-01 17:53:20 -0700

    Task 0, Pt. 3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,9 +8,6 @@ int main(void)
 {
 int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-
 if (n > 0)
 {
 printf("%d is postive\n", n);
@@ -21,5 +18,3 @@ printf("%d is zero\n", n);
 {
 printf("%d is negative\n", n);
 }
-
-return(0);
