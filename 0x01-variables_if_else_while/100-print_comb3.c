860f02ba9156357f853f46952eff2d202ad50373
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-01 19:59:23 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+
+/**
+ * main - Print all possible different combinations of two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int var;
+	for (var = 0; var < 9; var++)
+	{
+		int ii;
+		for (ii = var + 1; ii < 10; ii++)
+		{
+			putchar((var % 10) + '0');
+			putchar((ii % 10) + '0');
+			if (var != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
