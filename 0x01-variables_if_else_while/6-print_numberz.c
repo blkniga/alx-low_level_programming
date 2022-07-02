4f898acdb636af663be7164433d681167eea88b5
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-01 19:52:14 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - prints base ten digits without char
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
