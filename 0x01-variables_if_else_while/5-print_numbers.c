8bb98c24aad81491da6f49f40e295067a354254d
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-01 19:50:18 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Print digits of base ten
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
