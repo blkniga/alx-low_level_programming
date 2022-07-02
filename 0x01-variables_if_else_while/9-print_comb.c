498bbe60564e61f8c5054a516109675aabd9c685
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-01 19:56:47 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+/**
+ * main - Print possible combination 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i == '9')
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
