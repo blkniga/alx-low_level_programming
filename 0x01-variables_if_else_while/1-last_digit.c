c2add50b2c7cf64101256b97ed687634464342bd
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-01 18:28:54 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - Prints out different outputs depending on the variable n
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+if (n > 5)
+{
+	printf("Last string of %d is %d and is greater than 5", n, n);
+} else if (n == 0)
+{
+	printf("Last string of %d is %d and is 0", n, n);
+} else if (n < 6 && !0)
+{
+	printf("Last string of %d is %d and is less than 6 and not 0", n, n);
+}
+return (0);
+}
