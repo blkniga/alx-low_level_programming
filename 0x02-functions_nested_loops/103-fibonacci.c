f6f4cc19290880bc52b330a8720474fac916a6b6
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-06 08:50:02 -0700

    Task 103, Pt. 2

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,33 +1,20 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - Prints first 50 numbers of the fibonacci sequence
- *
+ * main - Prints sum of fibo numbers under 4,000,000
  * Return: 0
  */
 int main(void)
 {
-	int i, l;
-	long n1, n2, next;
+	long int f1 = 1, f2 = 2, sum = f2, tmp;
 
-	n1 = 1;
-	n2 = 2;
-	l = 50;
-
-	for (i = 0; i < l; i++)
+	while (f2 < 4000000)
 	{
-		if (i == 49)
-		{
-			printf("%lu", n1);
-		} else
-		{
-			printf("%lu, ", n1);
-			next = n1 + n2;
-			n1 = n2;
-			n2 = next;
-		}
+		tmp = f1 + f2;
+		f1 = f2;
+		f2 = tmp;
+		sum += (f2 % 2 == 0) ? f2 : 0;
 	}
-	printf("\n");
+	printf("%li\n", sum);
 	return (0);
 }
