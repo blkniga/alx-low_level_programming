95319c3a5ce750146d9cb907158b8becbb6076cc
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-06 08:07:22 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @number: The number passed as an argument
+ *
+ * Return: 0 if successful
+ */
+int print_last_digit(int number)
+{
+	return (number[-1]);
+}
