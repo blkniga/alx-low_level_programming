aee9fda5b6939e265cc1a8d7a0d7419900686565
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-06 07:47:56 -0700

    Task 8, Pt. 2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,13 +6,15 @@
  */
 void jack_bauer(void)
 {
-	for (int H = 0; H <= 2; H++)
+	int H, h, M, m;
+
+	for (H = 0; H <= 2; H++)
 	{
-		for (int h = 0; h <= 9; h++)
+		for (h = 0; h <= 9; h++)
 		{
-			for (int M = 0; M <= 5; M++)
+			for (M = 0; M <= 5; M++)
 			{
-				for (int m = 0; m <= 9; m++)
+				for (m = 0; m <= 9; m++)
 				{
 					_putchar(H);
 					_putchar(h);
