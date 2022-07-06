533d5bd804e744bae867e098acfbf6d803891e5f
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-06 08:31:13 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,5 +8,23 @@
  */
 int print_last_digit(int number)
 {
-	return (number[-1]);
+	int list[number - 1];
+	int list2[number - 1];
+	int i, j;
+
+	if (number > 0)
+	{
+		for (i = 0; i <= number; i++)
+		{
+			list[i] = i;
+		}
+		_putchar(list[-1]);
+	} else
+	{
+		for (j = 0; j <= number; j--)
+		{
+			list2[j] = j
+		}
+		_putchar(list[-1]);
+	}
 }
