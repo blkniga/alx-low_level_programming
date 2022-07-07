a466d746a9d0e6cb97a09c8431b86686f8736cba
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-07 16:46:58 -0700

    Task 0, Pt. 5

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _isupper(int c);
 int _putchar(char c);
@@ -13,6 +13,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-int main(void);
 
 #endif
