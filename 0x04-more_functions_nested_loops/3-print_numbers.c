70791c6a068094cc01272e9dfe2281d71cc4bb90
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-07 17:03:23 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers 0 - 9 followed by a new line
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
