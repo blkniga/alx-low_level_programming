fa3583848b10bc5e4fda1b652e0eecb4f013f1c3
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-07 18:30:30 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - Prints numbers 1-100 but multiples of 3 print Fizz,
+ * mulitples of 5 print buzz multiples of both print FizzBuzz
+ *
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5)
+		{
+			prinf("Buzz");
+		}
+		else if (i % 3 && i % 5)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+}
