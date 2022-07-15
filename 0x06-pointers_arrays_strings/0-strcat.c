423b13192a8f9d4378a3d693e6234aec1d845f24
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-14 18:16:50 -0700

    Task 0, Pt. 2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,10 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\n')
-	{
+	while (dest[i] != '\0')
 		i++;
-	}
 
-	while (src[j] != '\n')
+	while (src[j] != '\0')
 	{
 		*(dest + i) = src[j];
 		j++;
@@ -25,5 +23,6 @@ char *_strcat(char *dest, char *src)
 	}
 
 	*(dest + i) = '\0';
+
 	return (dest);
 }
