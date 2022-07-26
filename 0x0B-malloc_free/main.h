154795616cae74d22e8c4c27eb678848683cc3ac
Bulelani Botman <botmanbulelani@gmail.com>
2022-07-25 17:48:06 -0700

    Task 1, Pt. 3

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,7 +3,7 @@
 
 char **strtow(char *str);
 char *argstostr(int ac, char **av);
-void free_grid(int **grid, int height);'
+void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
