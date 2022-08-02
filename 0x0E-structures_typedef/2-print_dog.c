908be573869cf1c85b95fe7535f08583c6cc8edf
Bulelani Botman <botmanbulelani@gmail.com>
2022-08-01 17:51:21 -0700

    Task 2, Pt. 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,12 +8,10 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d)
 	{
-		printf("nil");
-	}
-	else
-	{
-		printf(d);
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
+		printf("Age: %s\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
